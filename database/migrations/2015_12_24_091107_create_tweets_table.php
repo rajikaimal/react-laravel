c5939749a45f5b85402d9501bad8c29ce27d4ca3
rajikaimal <inbox.rajika@gmail.com>
2015-12-25 00:28:01 +0530

    Add migration

---
+++ database/migrations/2015_12_24_091107_create_tweets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tweet');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tweets');
+    }
+}
